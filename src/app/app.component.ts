8d2d1317d60291386869636a1ef84f81ed466b42
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2017-01-30 20:38:20 -0500

    fixed mobile navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public isCollapsed:boolean = false;
+  public isCollapsed:boolean = true;
 
 }
