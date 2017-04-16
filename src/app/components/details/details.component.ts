fbd3aafeb38d3d2b90c23a62c12c300d68a13bc4
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2017-04-16 13:06:42 -0400

    updated details of wedding

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './details.component.html'
+  templateUrl: './details.component.html',
+  styleUrls: ['../../styles/shared.css']
 })
 export class DetailsComponent {
 
