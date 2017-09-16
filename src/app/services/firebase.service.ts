08c688d943b92396db9dcf1662af606b5f34cd0e
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2017-09-16 14:16:50 -0400

    added bar details

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -61,11 +61,6 @@ export class FirebaseService {
     this.household = this.af.database.object('/households/'+index) as
     FirebaseObjectObservable<Household>
     this.household.update({answers: answers});
-    // if (this.household[index].answers) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
   }
 
   getGuests() {
